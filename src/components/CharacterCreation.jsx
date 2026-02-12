36c07a6e4860a7639c89a11904f50ad9bd406ac3
Loic Rodewald <loic.rodewald@gmail.com>
2026-02-12 15:24:45 +0100

    feat: Ajout CSS CharacterCreation style livre de sorts ancien avec animations

--- src/components/CharacterCreation.jsx
+++ src/components/CharacterCreation.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { CLASSES, CLASS_CATEGORIES, ENRICHED_BACKSTORIES, getBackstoriesForClass, formatBackstoryForGM } from '../lore';
+import { MagicBackground } from './MagicBackground';
+import './CharacterCreation.css';
 
 // Utility: Roll 4d6 drop lowest
 const rollAttribute = () => {
