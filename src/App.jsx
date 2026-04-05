5dba4a44c62d6264c027ee94795b0e8b0d36da28
CodeVanta344 <sitewebtestswt@gmail.com>
2026-04-05 17:49:32 +0200

    Fix TDZ: simplify lazy import, remove m.CardCombat fallback

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import { LootModal } from './components/LootModal';
 import { NPCDialogueModal } from './components/NPCDialogueModal';
 import { DiceChallengeModal } from './components/DiceChallengeModal';
 import { CombatDistanceModal } from './components/CombatDistanceModal';
-const CardCombat = React.lazy(() => import('./components/CardCombat').then(m => ({ default: m.CardCombat ?? m.default })));
+const CardCombat = React.lazy(() => import('./components/CardCombat'));
 import { formatAIContent, calculateTotalStats, calculateMaxResource, resolvePlayerAbilities, generateArenaDecor, generateRandomCharacter, getArenaConfig } from './utils/gameUtils';
 import { AudioManager } from './components/AudioManager';
 import { GameHelperModal } from './components/GameHelperModal';
